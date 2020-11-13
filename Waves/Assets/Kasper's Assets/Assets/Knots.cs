d262a543715897baa7fa1b2b71b0166cf6489dd5
Kasper Amstrup Jensen <Kakko88@yahoo.dk>
2020-11-13 10:00:41 +0100

    rotter turning and speedometer

---
+++ Waves/Assets/Kasper's Assets/Assets/Knots.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Knots : MonoBehaviour
+{
+    public Rigidbody m_rigidbody;
+
+    [Header("UI")]
+    public Text speedLabel;
+
+    private float speed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        speed = m_rigidbody.velocity.magnitude * 1.944f;
+
+        if (speedLabel != null)
+            speedLabel.text = ((float)speed).ToString("F1") + " KN";
+    }
+}
---
+++ Waves/Assets/Kasper's Assets/Assets/Steering.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steering : MonoBehaviour
+{
+    public int turnSpeed;
+    public GameObject wheel;
+
+    float rotations = 0;
+    Quaternion defaultRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultRotation = wheel.transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            wheel.transform.Rotate(-Vector3.back * Time.deltaTime * turnSpeed);
+            rotations -= turnSpeed;
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+            wheel.transform.Rotate(Vector3.back * Time.deltaTime * turnSpeed);
+            rotations += turnSpeed;
+        }
+
+        else    //rotate to default
+        {
+            if (rotations < -1)
+            {
+                wheel.transform.Rotate(Vector3.back * Time.deltaTime * turnSpeed);
+                rotations += turnSpeed;
+            }
+            else if (rotations > 1)
+            {
+                wheel.transform.Rotate(-Vector3.back * Time.deltaTime * turnSpeed);
+                rotations -= turnSpeed;
+            }
+            else if (rotations != 0)
+            {
+                wheel.transform.localRotation = defaultRotation;
+                rotations = 0;
+            }
+        }
+    }
+}
