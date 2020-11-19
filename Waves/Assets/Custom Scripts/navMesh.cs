6a16ad096e254afa7e6423019ec4f4fa3782605f
Unknown <ndanie16@student.aau.dk>
2020-11-19 20:44:46 +0100

    Added AI boat

---
+++ Waves/Assets/Custom Scripts/navMesh.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class navMesh : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    public List<GameObject> checkpoints;
+
+    //remember to include using UnityEngine.AI;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(checkpoints[0].transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "AI_CheckpointOne")
+        {
+            agent.SetDestination(checkpoints[1].transform.position);
+        }
+        else if (other.name == "AI_CheckpointTwo")
+        {
+            agent.SetDestination(checkpoints[2].transform.position);
+        }
+        else if (other.name == "AI_CheckpointThree")
+        {
+            agent.SetDestination(checkpoints[3].transform.position);
+        }
+        else if (other.name == "AI_StartPoint")
+        {
+            agent.SetDestination(checkpoints[0].transform.position);
+        }
+    }
+}
---
+++ Waves/Assets/ResetAgent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetAgent : MonoBehaviour
+{
+    public Transform boat;
+    public Transform start;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "AI_Boat")
+        {
+            boat.position = start.position;
+            boat.rotation = start.rotation;
+        }
+    }
+}
